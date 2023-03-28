722f6731924b5ee41537c0558b8bf5dc8199fb9f
Max Zhang <0xC00005@gmail.com>
2023-03-28 17:13:55 -0400

    switch back to folium

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,48 @@
 import streamlit as st
 import pandas as pd
-from streamlit_leaflet import stleaflet
+import time
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import TimestampedGeoJson
+
 
 def process_csv_data(csv_string):
     df = pd.read_csv(pd.StringIO(csv_string), parse_dates=['timestamp'])
     return df
 
-def plot_movement(df, point_size):
-    center = [df['latitude'].mean(), df['longitude'].mean()]
-    zoom = 10
 
-    map_data = df[['latitude', 'longitude']].copy()
-    map_data['size'] = point_size
+def plot_movement(df, point_radius):
+    m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=13)
 
-    stleaflet.map(
-        center=center,
-        zoom=zoom,
-        data=map_data.to_dict(orient='records'),
-        draggable_circles=True,
-        radius_multiplier=1.0,
-    )
+    features = [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [row['longitude'], row['latitude']]
+            },
+            'properties': {
+                'times': [row['timestamp'].strftime('%Y-%m-%dT%H:%M:%SZ')],
+                'style': {'radius': point_radius, 'color': 'blue'}
+            }
+        }
+        for _, row in df.iterrows()
+    ]
+
+    TimestampedGeoJson(
+        {'type': 'FeatureCollection', 'features': features},
+        period='PT1M',
+        add_last_point=True,
+        auto_play=False,
+        loop=False,
+        max_speed=1,
+        loop_button=True,
+        date_options='YYYY-MM-DD HH:mm:ss',
+        time_slider_drag_update=True,
+    ).add_to(m)
+
+    folium_static(m)
+
 
 def main():
     st.set_page_config(page_title="GNSS Module Visualization", layout="wide")
@@ -31,12 +54,13 @@ def main():
 
     csv_input = st.text_area("Paste your CSV formatted text here:")
     display_button = st.button("Display")
-    point_size = st.slider("Point size", min_value=1, max_value=50, value=10, step=1)
 
     if display_button and csv_input:
         st.markdown("### Map Visualization")
         df = process_csv_data(csv_input)
-        plot_movement(df, point_size)
+        point_radius = st.slider("Point size", min_value=1, max_value=50, value=10)
+        plot_movement(df, point_radius)
+
 
 if __name__ == "__main__":
     main()
