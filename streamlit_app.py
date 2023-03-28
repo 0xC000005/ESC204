f156e29e3d50af409bca566abd5d7a261dafe003
Max Zhang <0xC00005@gmail.com>
2023-03-28 16:59:24 -0400

    switch to streamlit_leaflet

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,25 @@
 import streamlit as st
 import pandas as pd
-import time
-import folium
-from streamlit_folium import folium_static
-
+from streamlit_leaflet import stleaflet
 
 def process_csv_data(csv_string):
     df = pd.read_csv(pd.StringIO(csv_string), parse_dates=['timestamp'])
     return df
 
+def plot_movement(df, point_size):
+    center = [df['latitude'].mean(), df['longitude'].mean()]
+    zoom = 10
 
-def plot_movement(df, point_radius):
-    m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=12)
-    for index, row in df.iterrows():
-        folium.CircleMarker(
-            location=[row['latitude'], row['longitude']],
-            radius=point_radius,
-            color='red',
-            fill=True,
-            fill_color='red'
-        ).add_to(m)
-    folium_static(m)
+    map_data = df[['latitude', 'longitude']].copy()
+    map_data['size'] = point_size
 
+    stleaflet.map(
+        center=center,
+        zoom=zoom,
+        data=map_data.to_dict(orient='records'),
+        draggable_circles=True,
+        radius_multiplier=1.0,
+    )
 
 def main():
     st.set_page_config(page_title="GNSS Module Visualization", layout="wide")
@@ -33,13 +31,12 @@ def main():
 
     csv_input = st.text_area("Paste your CSV formatted text here:")
     display_button = st.button("Display")
+    point_size = st.slider("Point size", min_value=1, max_value=50, value=10, step=1)
 
     if display_button and csv_input:
         st.markdown("### Map Visualization")
         df = process_csv_data(csv_input)
-        point_radius = st.slider("Point size", min_value=1, max_value=50, value=5, step=1)
-        plot_movement(df, point_radius)
-
+        plot_movement(df, point_size)
 
 if __name__ == "__main__":
     main()
