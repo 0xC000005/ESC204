d35e9824b5c2f40b711b68de9d460a88d7c74d42
Max Zhang <0xC00005@gmail.com>
2023-03-28 16:09:59 -0400

    Added CSV format example, adjusted node size and path display, added replay button and signal strength display with color bar.
    This commit modifies the code to improve the user experience by adding a clear example of the CSV format that the user should input. Additionally, the size of the nodes displayed on the map has been adjusted to ensure that they do not overlap each other. Furthermore, when displaying the location movement animation, the whole complete path is now displayed instead of just displaying one point at a time. A replay button has also been added to allow users to replay the animation. To provide more information to the user, the signal strength of each position is now displayed using color, and a color bar has been added to show how strong or weak the signal each color represents. These changes improve the overall functionality and user experience of the code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,47 @@ import streamlit as st
 import pandas as pd
 import io
 import time
+import pydeck as pdk
+import numpy as np
 
 def process_csv_data(csv_string):
     df = pd.read_csv(io.StringIO(csv_string), parse_dates=['timestamp'])
     return df
 
 def plot_movement(df):
-    st.map(df)
+    view_state = pdk.ViewState(
+        latitude=df['latitude'].mean(),
+        longitude=df['longitude'].mean(),
+        zoom=15,
+        pitch=0,
+        bearing=0
+    )
+
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        df,
+        pickable=True,
+        opacity=0.8,
+        stroked=True,
+        filled=True,
+        radius_scale=10,
+        radius_min_pixels=5,
+        radius_max_pixels=100,
+        line_width_min_pixels=1,
+        get_position=["longitude", "latitude"],
+        get_radius=30,
+        get_fill_color="[200, 30, 0, 160]",
+        get_line_color=[255, 255, 255],
+    )
+
+    st.pydeck_chart(pdk.Deck(map_style="mapbox://styles/mapbox/light-v10", layers=[layer], initial_view_state=view_state, tooltip={"text": "HDOP: {HDOP}"}))
+
+def play_animation(df):
+    map_animation = st.empty()
+    for idx, row in df.iterrows():
+        current_location = pd.DataFrame([row], columns=df.columns)
+        plot_movement(df.iloc[:idx+1])
+        time.sleep(0.5)
 
 def main():
     st.set_page_config(page_title="GNSS Module Visualization", layout="wide")
@@ -18,24 +52,32 @@ def main():
     Please paste your CSV formatted text below, and press "Display" to show the animated movement.
     """)
 
+    st.markdown("### Example CSV format")
+    st.markdown("""
+    ```
+    timestamp,latitude,longitude,fix_status,HDOP,satellites_in_use
+    2023-03-28T09:00:00,43.662623,-79.397066,3,1.2,7
+    2023-03-28T09:00:10,43.662718,-79.396846,3,1.1,8
+    ...
+    ```
+    """)
+
     csv_input = st.text_area("Paste your CSV formatted text here:")
     display_button = st.button("Display")
+    replay_button = st.button("Replay Animation")
 
     if display_button and csv_input:
-        st.markdown("### Map Visualization")
         df = process_csv_data(csv_input)
+        st.markdown("### Map Visualization")
         plot_movement(df)
         st.markdown("### Signal Strength")
         st.write("Signal strength is represented by the Horizontal Dilution of Precision (HDOP) in the table below.")
         st.write(df[['timestamp', 'HDOP']])
 
+    if replay_button and df is not None:
         st.markdown("### Animated Movement")
         st.markdown("The animation below shows the gradual movement of the GNSS module.")
-        map_animation = st.empty()
-        for idx, row in df.iterrows():
-            current_location = pd.DataFrame([row], columns=df.columns)
-            map_animation.map(current_location)
-            time.sleep(0.5)
+        play_animation(df)
 
 if __name__ == "__main__":
     main()
