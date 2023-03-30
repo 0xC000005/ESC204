b8fae8427396dc48ab8a71d77bc134a2ec7e8ecb
Max Zhang <0xc00005@gmail.com>
2023-03-29 20:14:09 -0400

    code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,26 @@ if uploaded_file:
     # Filter options
     st.sidebar.header("Filter Options")
     start_date = st.sidebar.date_input("Start date", min_value=data["timestamp"].min().date())
-    end_date = st.sidebar.date_input("End date", max_value=data["timestamp"].max().date(), value=data["timestamp"].max().date())
-    fix_status_filter = st.sidebar.selectbox("Filter by fix status", options=sorted(data["fix_status"].unique()), index=0)
-    min_satellites = st.sidebar.slider("Minimum number of satellites in use", min_value=data["satellites_in_use"].min(), max_value=data["satellites_in_use"].max(), value=data["satellites_in_use"].min())
+    end_date = st.sidebar.date_input("End date", max_value=data["timestamp"].max().date(),
+                                     value=data["timestamp"].max().date())
+    fix_status_filter = st.sidebar.selectbox("Filter by fix status", options=sorted(data["fix_status"].unique()),
+                                             index=0)
+
+    if not data.empty:
+        min_satellites = st.sidebar.slider("Minimum number of satellites in use",
+                                           min_value=data["satellites_in_use"].min(),
+                                           max_value=data["satellites_in_use"].max(),
+                                           value=data["satellites_in_use"].min())
+    else:
+        st.sidebar.warning("No data found.")
+        min_satellites = None
 
     # Filter data
-    filtered_data = data[(data["timestamp"].dt.date >= start_date) & (data["timestamp"].dt.date <= end_date) & (data["fix_status"] == fix_status_filter) & (data["satellites_in_use"] >= min_satellites)]
+    if min_satellites is not None:
+        filtered_data = data[(data["timestamp"].dt.date >= start_date) & (data["timestamp"].dt.date <= end_date) & (
+                    data["fix_status"] == fix_status_filter) & (data["satellites_in_use"] >= min_satellites)]
+    else:
+        filtered_data = pd.DataFrame()
 
     # Display summary statistics
     st.header("Summary Statistics")
@@ -37,24 +51,27 @@ if uploaded_file:
     st.header("Filtered Data")
     st.write(filtered_data)
 
-    # Display map
-    st.header("Interactive Map Visualization")
-    map_center = [filtered_data["latitude"].mean(), filtered_data["longitude"].mean()]
-    m = folium.Map(location=map_center, zoom_start=14)
+    if not filtered_data.empty:
+        # Display map
+        st.header("Interactive Map Visualization")
+        map_center = [filtered_data["latitude"].mean(), filtered_data["longitude"].mean()]
+        m = folium.Map(location=map_center, zoom_start=14)
 
-    # Plot tracker path
-    path = filtered_data[["latitude", "longitude"]].values.tolist()
-    folium.PolyLine(path, color="blue", weight=5, opacity=0.7).add_to(m)
+        # Plot tracker path
+        path = filtered_data[["latitude", "longitude"]].values.tolist()
+        folium.PolyLine(path, color="blue", weight=5, opacity=0.7).add_to(m)
 
-    # Add markers for each point
-    for lat, lon, time in zip(filtered_data["latitude"], filtered_data["longitude"], filtered_data["timestamp"]):
-        folium.Marker(
-            location=[lat, lon],
-            icon=None,
-            popup=f"Time: {time}"
-        ).add_to(m)
+        # Add markers for each point
+        for lat, lon, time in zip(filtered_data["latitude"], filtered_data["longitude"], filtered_data["timestamp"]):
+            folium.Marker(
+                location=[lat, lon],
+                icon=None,
+                popup=f"Time: {time}"
+            ).add_to(m)
 
-    folium_static(m)
+        folium_static(m)
+    else:
+        st.warning("No filtered data to display")
 else:
     st.warning("Please upload a GNSS log file in CSV format")
     st.stop()
