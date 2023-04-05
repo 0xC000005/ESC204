502523b2d6ae0dd331c47f32a02deae14ec8577f
Max Zhang <0xC00005@gmail.com>
2023-04-05 14:36:58 -0400

    create the gps location shifter program

--- adjust_gps.py
+++ adjust_gps.py
@@ -1,35 +1,40 @@
 import sys
 import csv
 
-def adjust_gps(input_file, output_file, correct_latitude, correct_longitude):
+
+def adjust_gps(input_file, output_file, correct_latitude, correct_longitude, factor):
     with open(input_file, 'r') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
 
         data = [row for row in reader]
 
-    original_latitude = float(data[0][1]) # Change this to 1
-    original_longitude = float(data[0][2]) # Change this to 2
-
-    lat_diff = correct_latitude - original_latitude
-    lon_diff = correct_longitude - original_longitude
+    original_latitude = float(data[0][1])
+    original_longitude = float(data[0][2])
 
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
 
         for row in data:
-            adjusted_latitude = float(row[1]) + lat_diff # Change this to 1
-            adjusted_longitude = float(row[2]) + lon_diff # Change this to 2
+            latitude = float(row[1])
+            longitude = float(row[2])
+
+            # Calculate the stretched latitude and longitude
+            adjusted_latitude = correct_latitude + (latitude - correct_latitude) * factor
+            adjusted_longitude = correct_longitude + (longitude - correct_longitude) * factor
+
             writer.writerow([row[0], adjusted_latitude, adjusted_longitude] + row[3:])
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python adjust_gps.py input_file output_file correct_latitude correct_longitude")
+    if len(sys.argv) != 6:
+        print("Usage: python adjust_gps.py input_file output_file correct_latitude correct_longitude factor")
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         correct_latitude = float(sys.argv[3])
         correct_longitude = float(sys.argv[4])
+        factor = float(sys.argv[5])
 
-        adjust_gps(input_file, output_file, correct_latitude, correct_longitude)
+        adjust_gps(input_file, output_file, correct_latitude, correct_longitude, factor)
