8d8b690ad9df997eae99f0da9d99223ef3205f0d
Will Taylor <93415447+wptaylor@users.noreply.github.com>
2023-04-09 11:47:19 -0400

    Update simulateSignalStrength.m
    Fixed frequency to 2.45GHz, changed W to mW.

--- simulateSignalStrength.m
+++ simulateSignalStrength.m
@@ -1,18 +1,18 @@
 function receivedPowerDbm = simulateSignalStrength(transmitterPowerDbm, transmitterGain, receiverGain, distances, attenuationFactors)
     % Constants
-    speedOfLight = 3e8; % Speed of light in meters/second
-    frequency = 1575.42e6; % L1 GPS frequency in Hz
+    speedOfLight = 2.998e8; % Speed of light in meters/second
+    frequency = 2.45e9; % L1 GPS frequency in Hz
     wavelength = speedOfLight / frequency;
 
-    % Convert transmitter power to watts
-    transmitterPowerWatts = 10^((transmitterPowerDbm - 30) / 10);
+    % Convert transmitter power to milliwatts
+    transmitterPowerMWatts = 10^(transmitterPowerDbm / 10);
 
     % Calculate the received power in watts using the free space path loss formula
-    receivedPowerWattsFreeSpace = (transmitterPowerWatts * (10^(transmitterGain / 10)) * (10^(receiverGain / 10)) * (wavelength .^ 2)) ./ ((4 * pi * distances) .^ 2);
+    receivedPowerMWattsFreeSpace = (transmitterPowerWatts * (10^(transmitterGain / 10)) * (10^(receiverGain / 10)) * (wavelength .^ 2)) ./ ((4 * pi * distances) .^ 2);
 
     % Apply attenuation factor
     receivedPowerWatts = receivedPowerWattsFreeSpace .* attenuationFactors;
 
     % Convert received power back to dBm
-    receivedPowerDbm = 10 * log10(receivedPowerWatts) + 30;
+    receivedPowerDbm = 10 * log10(receivedPowerWatts);
 end
